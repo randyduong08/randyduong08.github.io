e5d551ea21649a54b05111d2206c94dff2bee710
randyduong08 <randyduong08@gmail.com>
2023-03-04 22:48:21 -0500

    Dark Mode implement
    Changed a bunch of files to basically implement the ability to switch between dark mode and light mode on the page

--- index.js
+++ index.js
@@ -2,3 +2,15 @@ function redirectToProjects(event){
     event.preventDefault();
     window.location.href = "projects.html";
 }
+
+function toggleDarkMode() {
+    const body = document.body;
+    const headerFooter = document.querySelectorAll('.header-footer');
+    
+    body.classList.toggle('dark');
+    headerFooter.forEach(tag => tag.classList.toggle('dark-header-footer'));
+
+  }
+const modeButton = document.getElementById("mode-button");
+
+modeButton.addEventListener('click', toggleDarkMode);
