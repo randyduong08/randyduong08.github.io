08c6d95d54b430621bf971c24f4f278f683c085d
randyduong08 <randyduong08@gmail.com>
2023-03-04 22:58:15 -0500

    Updated modebutton
    Made it so the top-right button now switched value between 'Dark Mode' and 'Light Mode'

--- index.js
+++ index.js
@@ -3,14 +3,25 @@ function redirectToProjects(event){
     window.location.href = "projects.html";
 }
 
-function toggleDarkMode() {
+function toggleDarkMode(event, form) {
+    event.preventDefault();
     const body = document.body;
     const headerFooter = document.querySelectorAll('.header-footer');
+    const modeButton = form.querySelector('input[type="submit"]');
     
     body.classList.toggle('dark');
     headerFooter.forEach(tag => tag.classList.toggle('dark-header-footer'));
 
+    if (modeButton.value === 'Dark Mode'){
+        modeButton.value = 'Light Mode';
+    }
+    else{
+        modeButton.value = 'Dark Mode';
+    }
+
   }
+/*
 const modeButton = document.getElementById("mode-button");
 
 modeButton.addEventListener('click', toggleDarkMode);
+*/
