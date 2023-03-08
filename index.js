2af59d10d622f6fe28b35dd36b905361dc88442f
randyduong08 <randyduong08@gmail.com>
2023-03-07 21:28:24 -0500

    Update index.js
    Implementation feature for localstorage to save memory of user's theme preference

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ function redirectToProjects(event){
     window.location.href = "projects.html";
 }
 
+//Keep track of current style (either light.css or dark.css) for the document
+var style = document.getElementById("style");
+
 function toggleTheme(){
-    var style = document.getElementById("style");
     if (style.href.match("light.css")){
         style.href = "dark.css";
     } else{
@@ -13,7 +15,24 @@ function toggleTheme(){
 }
 
 const modeLink = document.getElementById("switch-theme");
-let mode = "dark";
+
+
+// Get current mode from local storage
+var mode = localStorage.getItem("mode");
+
+// If mode is not set (so no preference), default to mode=dark, i.e., in light mode (confusing)
+if (mode == null || mode=="dark"){
+    mode = "dark";
+    modeLink.textContent = "Dark Mode";
+    style.href = "light.css";
+}
+//Otherwise, set stuff to dark mode
+else{
+    mode = "light";
+    modeLink.textContent = "Light Mode";
+    style.href = "dark.css";
+}
+
 
 modeLink.addEventListener("click", () => {
     if (mode == "dark"){
@@ -24,6 +43,7 @@ modeLink.addEventListener("click", () => {
         modeLink.textContent = "Dark Mode";    
         mode = "dark";
     }
+    localStorage.setItem("mode", mode);
     toggleTheme();
 });
 
