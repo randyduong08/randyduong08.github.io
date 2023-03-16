55f7166fd3221f402651d1460d0b3fe15a4ee9f6
randyduong08 <randyduong08@gmail.com>
2023-03-16 19:22:29 -0400

    js changes
    first implementation of moving project cards by mouse dragging

--- projects.js
+++ projects.js
@@ -1,11 +1,39 @@
-//grab the card container in project.html
-const track = document.getElementById("container");
+    //grab the card container in project.html
+const track = document.getElementById("card-track");
 
+console.log(track);
+
+const handleOnDown = e => track.dataset.mouseDownAt = e.clientX;
+
+console.log(handleOnDown);
+
+const handleOnUp = () => {
+    track.dataset.mouseDownAt = "0";
+    track.dataset.prevPercentage = track.dataset.percentage;
+}
 
 //track position of curser on mousedown
-window.onmousedown = e => {
+const handleOnMove = e => {
+    if(track.dataset.mouseDownAt == "0") return;
+
+    console.log('test');
+
     const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
         maxDelta = window.innerWidth / 2;
     
-    const percentage = (mouseDelta / maxDelta) * 100;
+    const percentage = (mouseDelta / maxDelta) * -100,
+        nextPercentageUnconstrained = 
+            parseFloat(track.dataset.prevPercentage) + percentage,
+        nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+
+    track.dataset.percentage = nextPercentage;
+
+    track.animate({
+        transform: 'translate(${nextPercentage}%, -50%)'},
+        { duration: 1200, fill: "forwards"});
+    
+    for(const image of track.getElementsByClassName("card-img-top")){
+        image.animate({objectPosition: '${100 + nextPercentage}% center'},
+                    {duration: 1200, fill: "forwards"});
+    }
 }
