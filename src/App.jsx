b7ca6bc531a88fae27c2a0310b7e1349edcbd34d
randyduong08 <randyduong08@gmail.com>
2023-03-21 22:53:37 -0400

    Implemented React-router-dom
    Effectively made it so you can separate different pages into their own folders, and adding it to the Main.jsx will allow for navigation between the pages
    
    TODO: add button functionality to navigate to the pages, fix images on the project cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
-//Combine all index components in this one
-import Header from "./Header";
-import MainSection from "./MainSection";
-import Footer from "./Footer";
 import Main from "./Main";
 
 function App(){
     return(
         <div>
-            <Header />
-                <MainSection />
-            <Footer />
+            <Main/>
         </div>
     );
 }
