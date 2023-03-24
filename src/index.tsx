350df4070b5fe1d54b9f6df2a1fc328db6fdbfd0
randyduong08 <randyduong08@gmail.com>
2023-03-23 21:50:46 -0400

    Start TS conversion
    Currently starting the conversion from JSX to TSX, one file at a time
    
    Startingg with index.jsx -> index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -5,10 +5,13 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
+const rootElement = document.getElementById('root');
+if (rootElement != null){
+    const root = ReactDOM.createRoot(rootElement);
+    root.render(
         <BrowserRouter>
-            <App /> 
+            <App />
         </BrowserRouter>
-);
+    );
+}
 
