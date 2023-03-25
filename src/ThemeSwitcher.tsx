7dfd9b5a4432cf10909ac4b1726a7d8eef20d447
randyduong08 <randyduong08@gmail.com>
2023-03-24 21:47:58 -0400

    changed

--- src/ThemeSwitcher.tsx
+++ src/ThemeSwitcher.tsx
@@ -3,37 +3,37 @@ import React, { useState, useEffect } from "react";
 type Theme = "light" | "dark";
 
 const ThemeSwitcher: React.FC = () => {
-    const [theme, setTheme] = useState<Theme>("light");
+  const [theme, setTheme] = useState<Theme>("light");
 
-    useEffect(() => {
-        const savedTheme = localStorage.getItem("theme");
-        if (savedTheme){
-            setTheme(savedTheme as Theme);
-        }
-    }, []);
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme) {
+      setTheme(savedTheme as Theme);
+    }
+  }, []);
 
-    const toggleTheme = () => {
-        const newTheme = theme === "light" ? "dark" : "light";
-        setTheme(newTheme);
-        localStorage.setItem("theme", newTheme);
-    };
+  const toggleTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+    localStorage.setItem("theme", newTheme);
+  };
 
-    const loadTheme = () => {
-        const themeLink = document.getElementById("theme-link") as HTMLLinkElement;
-        if (themeLink){
-            themeLink.href = '/${theme}.css';
-        }
-    };
+  const loadTheme = () => {
+    const link = document.querySelector('link[rel="stylesheet"]') as HTMLLinkElement;
+    if (link){
+        link.href = `/${theme}.css`;
+    }
+  };
 
-    useEffect(() => {
-        loadTheme();
-    }, [theme]);
+  useEffect(() => {
+    loadTheme();
+  }, [theme]);
 
-    return (
-        <button onClick={toggleTheme}>
-            {theme === "light" ? "Dark Mode" : "Light Mode"}
-        </button>
-    );
+  return (
+    <button onClick={toggleTheme}>
+      {theme === "light" ? "Dark Mode" : "Light Mode"}
+    </button>
+  );
 };
 
 export default ThemeSwitcher;
