21a5a2d357b4c917235240cd89adfaec04c791b3
randyduong08 <randyduong08@gmail.com>
2023-03-25 16:41:00 -0400

    Moved Nav bar
    Moved nav bar from being individually in each type of page, so simply begin in the 'src' folder, appearing in the App.tsx (i.e. Navbar is consistent across all pages)
    
    Reasons for this:
    - Removes confusion with having duplicate headers and nav bars
    - Theoretically improves runtime?
    - Prevents reloading of savedtheme when moving between pages, which can cause annoying flashes of white to the user when in dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Main from "./Main";
 import React from "react";
+import Header from "./Header";
 
 function App(){
     return(
         <div>
+            <Header/>
             <Main/>
         </div>
     );
