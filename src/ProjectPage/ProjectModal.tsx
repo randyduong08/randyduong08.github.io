5950e569b7becd11233c543fd0218c7a066092bf
randyduong08 <randyduong08@gmail.com>
2023-03-29 19:27:26 -0400

    stuff
    theme related stuff

--- src/ProjectPage/ProjectModal.tsx
+++ src/ProjectPage/ProjectModal.tsx
@@ -9,13 +9,13 @@ interface ProjectModalProps{
 
 const ProjectModal = ({ show, handleClose }: ProjectModalProps) => {
   return (
-    <Modal show={show} onHide={handleClose} centered>
+    <Modal show={show} onHide={handleClose} centered className="project-modal">
       <Modal.Header closeButton>
         <Modal.Title>Project Details</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <h5>Project Title</h5>
-        <p>Sample text explaining the purpose of this project</p>
+        <h5 className="modal-text">Project Title</h5>
+        <p className="modal-text">Sample text explaining the purpose of this project</p>
       </Modal.Body>
       <Modal.Footer>
         <Button variant="secondary" onClick={handleClose}>
