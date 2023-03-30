ba2f788d80531adacedbf15b4c9097139709dbe1
randyduong08 <randyduong08@gmail.com>
2023-03-29 20:33:25 -0400

    Implemented ThemeContext for modal
    modal box now has working theme color changes

--- src/ProjectPage/ProjectModal.tsx
+++ src/ProjectPage/ProjectModal.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useContext } from "react";
 import styled from "styled-components";
 import { Modal, Button } from "react-bootstrap";
+import { ThemeContext } from "src/ThemeContext";
 
 interface ProjectModalProps{
     show: boolean;
@@ -8,16 +9,20 @@ interface ProjectModalProps{
 }
 
 const ProjectModal = ({ show, handleClose }: ProjectModalProps) => {
+  const { theme } = useContext(ThemeContext);
+
   return (
     <Modal show={show} onHide={handleClose} centered className="project-modal">
-      <Modal.Header closeButton>
+      <Modal.Header closeButton className="modal-box">
         <Modal.Title>Project Details</Modal.Title>
       </Modal.Header>
-      <Modal.Body>
-        <h5 className="modal-text">Project Title</h5>
-        <p className="modal-text">Sample text explaining the purpose of this project</p>
+      <Modal.Body className="modal-box">
+        <StyledText theme={theme} className="modal-text">
+          <h5>Project Title</h5>
+          <p>Sample text explaining the purpose of this project</p>
+        </StyledText>
       </Modal.Body>
-      <Modal.Footer>
+      <Modal.Footer className="modal-box">
         <Button variant="secondary" onClick={handleClose}>
           Close
         </Button>
@@ -25,5 +30,9 @@ const ProjectModal = ({ show, handleClose }: ProjectModalProps) => {
     </Modal>
   );
 };
+
+const StyledText = styled.div<{ theme: "light" | "dark" }>`
+  color: ${({ theme }) => (theme === "light" ? "#333" : "#ffffff")};
+`;
 
 export default ProjectModal;
