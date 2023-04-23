307b58092296ef882bd7736ac1262ac3f18c9740
randyduong08 <randyduong08@gmail.com>
2023-04-23 17:19:37 -0400

    Updated ProjectsContainer.tsx
    Changed implementation of project data in the ...Container.tsx:
    
    Instead of hardcoded a new <ProjectCard/> for every card, use a mapping to a list of data instead, 'projectData'

--- src/ProjectPage/ProjectsContainer.tsx
+++ src/ProjectPage/ProjectsContainer.tsx
@@ -2,6 +2,33 @@ import React from "react";
 import ProjectCard from "./ProjectCard";
 import pimg from '../Images/nightsky.jpg';
 
+const projectData = [
+    {
+        title:"Twitch AI Outcome Predictor",
+        description:"Predict the outcome of MK games on Twitch streams",
+        imgSrc:pimg,
+        githubLink:"https://github.com/randyduong08",
+        modalTitle:"Outcome Predictor",
+        modalDesc:"Python program that uses regressional model to predict the outcome of Mario Kart games on Twitch streams. Uses OpenCV and pytesseract, for Computer Vision and OCR respectively."
+    },
+    {
+        title:"Real-Time Occupancy Monitoring System",
+        description:"Monitors and actively updates the status of people/pets",
+        imgSrc:pimg,
+        githubLink:"https://github.com/randyduong08",
+        modalTitle:"Monitoring System",
+        modalDesc:"Python Program that monitors the number of people inside/outside the house. Using regression models to predict the activities of people when they go out. Uses YOLO to detect familiar people in/out the household."
+    },
+    {
+        title:"Unity Game Project",
+        description:"Multiplayer game in Unity",
+        imgSrc:pimg,
+        githubLink:"https://github.com/randyduong08",
+        modalTitle:"Unity Game",
+        modalDesc:"Developed a game in Unity, that has network capabilities for multiplayer with sever-client or peer-to-peer connection."
+    }
+]
+
 const ProjectsContainer = () => {
     return(
         <div className="scroll-container">
@@ -15,41 +42,19 @@ const ProjectsContainer = () => {
             <div className="row">
                 <div className="col-md-1" />
                 <div className="col-md-10">
-                {/*Project cards go here, in the middle main column*/}
-                {/*test new card*/}
-                <ProjectCard
-                    title="Twitch AI Outcome Predictor"
-                    description="Predict the outcome of MK games on Twitch streams"
-                    imgSrc={pimg}
-                    githubLink="https://github.com/randyduong08"
-                    modalTitle="Outcome Predictor"
-                    modalDesc="Python program that uses regressional model to predict 
-                                the outcome of Mario Kart games on Twitch streams.
-                                Uses OpenCV and pytesseract, for Computer Vision and
-                                OCR respectively."
-                    index = {0}
-                />
-                <ProjectCard
-                    title="Real-Time Occupancy Monitoring System"
-                    description="Monitors and actively updates the status of people/pets"
-                    imgSrc={pimg}
-                    githubLink="https://github.com/randyduong08"
-                    modalTitle="Monitoring System"
-                    modalDesc="Python Program that monitors the number of people inside/outside the house.
-                                Using regression models to predict the activities of people when they go out.
-                                Uses YOLO to detect familiar people in/out the household."
-                    index = {1}
-                />
-                <ProjectCard
-                    title="Unity Game Project"
-                    description="Multiplayer game in Unity"
-                    imgSrc={pimg}
-                    githubLink="https://github.com/randyduong08"
-                    modalTitle="Unity Game"
-                    modalDesc="Developed a game in Unity, that has network capabilities for multiplayer with sever-client or peer-to-peer connection."
-                    index = {2}
-                />
-                {/*test new card above*/}
+                {/*Mapping of project data here*/}
+                {projectData.map((project, index) => (
+                    <ProjectCard 
+                        key={index}
+                        index={index}
+                        title={project.title}
+                        description={project.description}
+                        imgSrc={project.imgSrc}
+                        githubLink={project.githubLink}
+                        modalTitle={project.modalTitle}
+                        modalDesc={project.modalDesc}
+                    />
+                ))}
                 </div>
                 <div className="col-md-1" />
             </div>
