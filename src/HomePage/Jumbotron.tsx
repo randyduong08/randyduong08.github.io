e4a0ab6060850bc4654c08ca06f2575524c1301a
randyduong08 <randyduong08@gmail.com>
2023-04-23 21:59:29 -0400

    Update Jumbotron.tsx
    Added StyledComponent button that when clicked, will navigate to projects page

--- src/HomePage/Jumbotron.tsx
+++ src/HomePage/Jumbotron.tsx
@@ -1,26 +1,57 @@
 import React from "react";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
 //Jumbotron component
 // Uses bootstrap jumbotron class
 
-function Jumbotron() {
-    return (
-        <div className="jumbotron">
-  <div className="container text-center">
-    <h1>Randy Duong</h1>
-    <div className="intro-text">
-      <p>3rd year back-end software developer</p>
-      <p>sample text</p>
-      <p>sample text</p>
-    </div>
-    <form id="mainbutton" onSubmit={(event) => {
-      event.preventDefault();
-      console.log('test');
-    }}>
-      <input type="submit" defaultValue="Check out my projects" />
-    </form>
-  </div>
-</div>
-    )
+interface ButtonProps {
+  onClick: () => void;
+  text: string;
 }
+
+function Button(props: ButtonProps) {
+  return <StyledButton onClick={props.onClick}>{props.text}</StyledButton>
+}
+
+function Jumbotron() {
+
+  let navigate = useNavigate();
+
+  const handleButtonClick = () => {
+    navigate("/Projects");
+  }; 
+
+  return (
+    <div className="jumbotron">
+      <div className="container text-center">
+        <h1>Randy Duong</h1>
+        <div className="intro-text">
+          <p>3rd year back-end software developer</p>
+          <p>sample text</p>
+          <p>sample text</p>
+        </div>
+        <Button onClick={handleButtonClick} text="Check out my work" />
+      </div>
+    </div>
+  )
+}
+
+const StyledButton = styled.button`
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  padding: 10px 20px;
+  font-size: 1rem;
+  font-weight: bold;
+  cursor: pointer;
+  transition: all 0.3s ease;
+
+  &:hover {
+    background-color: #0069d9;
+  }
+
+`;
+
 
 export default Jumbotron;
