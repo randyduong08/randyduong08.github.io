65fdcbc61fd190ecedb688a92b98b603f0ab3949
randyduong08 <randyduong08@gmail.com>
2023-04-23 22:40:15 -0400

    Update Jumbotron.tsx
    Added more styling to button, to look more cool

--- src/HomePage/Jumbotron.tsx
+++ src/HomePage/Jumbotron.tsx
@@ -10,7 +10,11 @@ interface ButtonProps {
 }
 
 function Button(props: ButtonProps) {
-  return <StyledButton onClick={props.onClick}>{props.text}</StyledButton>
+  return(
+    <StyledButton onClick={props.onClick} data-text={props.text}>
+      {props.text}
+    </StyledButton>
+  );
 }
 
 function Jumbotron() {
@@ -37,20 +41,59 @@ function Jumbotron() {
 }
 
 const StyledButton = styled.button`
-  background-color: #007bff;
-  color: #fff;
+  position: relative;
+  background-color: rgba(211, 211, 211, 0.3);
+  color: #007bff;
   border: none;
-  border-radius: 5px;
   padding: 10px 20px;
   font-size: 1rem;
   font-weight: bold;
   cursor: pointer;
-  transition: all 0.3s ease;
+  overflow: hidden;
+  transform: skewX(-15deg);
 
-  &:hover {
-    background-color: #0069d9;
+  &:before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: -150%;
+    width: 150%;
+    height: 100%;
+    background-color: #add8e6;
+    transform: skewX(0deg);
+    transition: all 0.3s ease;
+    opacity: 0;
   }
 
+  &:after {
+    content: attr(data-text);
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    color: #000;
+    padding: 10px 20px;
+    font-size: 1rem;
+    font-weight: bold;
+    transform: skewX(-15deg);
+    clip-path: polygon(0 0, 0% 100%, 0% 100%, 0 0);
+    transition: all 0.3s ease;
+  }
+
+  &:hover {
+    color: transparent;
+  }
+
+  &:hover:before {
+    left: 0;
+    opacity: 1;
+    transition: all 0.3s ease, opacity 0s;
+  }
+
+  &:hover:after {
+    clip-path: polygon(0 0, 100% 0, 100% 100%, 0 100%);
+  }
 `;
 
 
