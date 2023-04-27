7459bf88929480f9b214b17c7f560070677c6add
randyduong08 <randyduong08@gmail.com>
2023-04-26 22:41:27 -0400

    Update Footer.tsx
    Added animation for footer to slide-up from out of screen, after all content on main page has loaded in

--- src/HomePage/Footer.tsx
+++ src/HomePage/Footer.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import resumePDF from "../externalfiles/Resume.pdf";
+import styled, { keyframes } from "styled-components";
 //Footer component
 
 
 function Footer(){
     return(
-        <footer className="header-footer">
+        <StyledFooter className="header-footer">
         <div className="row">
           <div className="col-sm-4">
             <h6>Contact information</h6>
@@ -53,9 +54,26 @@ function Footer(){
             </p>
           </div>
         </div>
-      </footer>
+      </StyledFooter>
     );
 
 }
 
 export default Footer;
+
+const slideUp = keyframes`
+  0% {
+    opacity: 0;
+    transform: translateY(100%);
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(0);
+  }
+`;
+
+const StyledFooter = styled.footer`
+  opacity: 0;
+  animation: ${slideUp} 1s forwards;
+  animation-delay: 1.8s; //set to 0.3 seconds after last Main page fade-in animation
+`;
